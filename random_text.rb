accaeefca1128f4e8cf518615317311a908f8f63
Cassie Moy <moy.cassie@gmail.com>
2015-08-07 19:30:27 -0400

    Able to clean content and build random sentence.

--- random_text.rb
+++ random_text.rb
@@ -1,8 +1,26 @@
-# read text from file in directory
-
-Dir.entries(".").each do |i|
-  if File.extname(i) == ".txt"
-    p i
-    p "is text"
-  end
+def build_paragraph()
 end
+
+def build_sentence(words)
+  sentence = words.sample(10)
+  return sentence
+end
+
+def generate()
+  input = '';
+
+  Dir.entries('.').each do |i|
+    if File.extname(i) == '.txt'
+      input = i
+    end
+  end
+
+  content = File.read(input)
+  all_words = []
+  content.split(/\s/).each { |word| all_words << word.gsub(/\W/, '').downcase }
+  
+  p build_sentence(all_words)
+
+end
+
+generate()
