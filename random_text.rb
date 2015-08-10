09ab9db10def1395c0266ac9d1b07394ce36c46f
Cassie Moy <moy.cassie@gmail.com>
2015-08-10 02:31:11 -0400

    First working solution.

--- random_text.rb
+++ random_text.rb
@@ -1,20 +1,20 @@
 def build_paragraph(words)
   paragraph = []
-  10.times { paragraph << words.sample(10).join(" ").capitalize + '.' }
+  10.times { paragraph << words.sample(10).join(" ").capitalize + "." }
   return paragraph.join(" ")
 end
 
 def wrap_text(paragraph)
-  line = ''
+  line = ""
 
   paragraph.split(" ").each do |word|
     new_length = line.length + word.length + 1
 
     if line.length <= 80 && new_length > 80
       print line + "\n"
-      line = word + ' '
+      line = word + " "
     else
-      line += word + ' '
+      line += word + " "
     end
   end
 
@@ -22,22 +22,20 @@ def wrap_text(paragraph)
 end
 
 def generate()
-  input = '';
+  all_words = []
 
-  Dir.entries('.').each do |i|
-    if File.extname(i) == '.txt'
-      input = i
+  loop do
+    Dir.entries('.').each do |file|
+      if File.extname(file) == '.txt'
+        input = File.read(file)
+        input.split(/\s/).each { |word| all_words << word.gsub(/\W/, "").downcase }
+        all_words.delete("")
+        paragraph = build_paragraph(all_words)
+        wrap_text(paragraph)
+        File.delete(file)
+      end
     end
   end
-
-  content = File.read(input)
-  all_words = []
-  content.split(/\s/).each { |word| all_words << word.gsub(/\W/, '').downcase }
-  all_words.delete("")
-  paragraph = build_paragraph(all_words)
-  wrap_text(paragraph)
 end
 
-loop do
-  generate()
-end
+generate()
