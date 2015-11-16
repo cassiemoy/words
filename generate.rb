2721b4c4bef5fdf4d79819cf0f793417a8b4e63d
Cassie Moy <moy.cassie@gmail.com>
2015-11-16 12:47:54 -0500

    Interview challenge for PD.

--- generate.rb
+++ generate.rb
@@ -1,24 +1,49 @@
 def build_paragraph(words)
   paragraph = []
   10.times { paragraph << words.sample(10).join("\s").capitalize + "." }
-  return paragraph.join(" ")
+  return paragraph.join("\s")
 end
 
-def wrap_text(paragraph)
+def wrap_text(paragraph, limit)
+  output = []
   line = ""
-
   paragraph.split(/\s/).each do |word|
+    while word.length > limit
+      line = word.slice!(0..(limit - 1))
+    end
+    
     new_length = line.length + word.length + 1
 
-    if line.length <= 80 && new_length > 80
-      print line + "\n"
+    if line.length <= limit && new_length > limit
+      output << line
       line = word + "\s"
     else
       line += word + "\s"
     end
   end
 
-  print line + "\n"
+  output << line
+  return output
+end
+
+def print_text(output, limit)
+  output.each do |line|
+    print center_text(line, limit) + "\n"
+    print right_align_text(line, limit) + "\n"
+    print line + "\n"
+  end
+end
+
+def center_text(line, limit)
+  spaces = limit - line.length
+  line = "\s" * (spaces/2) + line + "\s" * (spaces/2)
+  return line
+end 
+
+def right_align_text(line, limit)
+  spaces = limit - line.length
+  line = "\s" * spaces + line
+  return line
 end
 
 def generate()
@@ -33,7 +58,9 @@ def generate()
           all_words << word.gsub(/\W/, "").downcase if word != ""
         end
         
-        wrap_text(build_paragraph(all_words))
+        limit = 40
+        output = wrap_text(build_paragraph(all_words), limit)
+        print_text(output, limit)
         File.delete(file)
       end
     end
