855501acef58eecd8b0a6a629000a5f6343a5029
Cassie Moy <cassiemoy@positiondev.com>
2015-11-16 17:41:16 -0500

    Read from file to list, lowercase, and remove punctuation.

---
+++ generate.hs
@@ -0,0 +1,17 @@
+import Data.Char
+import Data.List
+import Text.Printf
+
+import System.IO
+
+lowercase :: String -> String
+lowercase = map toLower
+
+removePunctuation :: [String] -> [String]
+removePunctuation = map (filter isAlpha)
+
+main = do 
+    content <- readFile "subjective.txt"
+    let list = removePunctuation(words (lowercase (content)))
+    
+    print list
