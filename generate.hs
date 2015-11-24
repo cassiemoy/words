4cc0cdb5ece91ba9d17365a9dfe7971be6226e7b
Cassie Moy <cassiemoy@positiondev.com>
2015-11-24 16:47:04 -0500

    :Wrote out map to understand it. EOD commit, working on building paragraph.

--- generate.hs
+++ generate.hs
@@ -2,6 +2,33 @@ import Data.Char
 import Data.List
 import Text.Printf
 import System.IO
+import System.Random
+
+-- an extremely annotated attempt @ words
+
+-- map takes a function (that takes a and returns b), [a], and returns [b]
+myMap :: (a->b) -> [a] -> [b]
+myMap _ [] = []
+myMap f (x:xs) = f x : myMap f xs
+
+-- : is cons operator, appends a head element to a list, x onto xs
+-- (x:xs) is a pattern for nonempty lists
+-- _ matches anything without binding, f matches anything with binding?
+-- base case is empty list
+-- recursive case is match first list element x, return (f x): map f xs
+-- map f [x1, x2, ... ] == [f x1, f x2, ...]
+-- recursion, pattern matching, partial function application + currying
+
+-- mapM :: (a -> IO b) -> [a] -> IO [b]
+
+buildParagraph :: (String -> IO String) -> [String] -> IO [String]
+buildParagraph = 
+
+randomWord :: [String] -> IO String
+randomWord words = do
+  let wordcount = length(words)
+  randomIndex <- randomRIO(0, wordcount - 1)
+  return (words !! randomIndex)
 
 lowercase :: String -> String
 lowercase = map toLower
@@ -9,8 +36,11 @@ lowercase = map toLower
 removePunctuation :: [String] -> [String]
 removePunctuation = map (filter isAlpha)
 
+main :: IO String
 main = do 
     content <- readFile "subjective.txt"
     let list = removePunctuation(words (lowercase (content)))
-    
-    print list
+
+    --mapM randomWord words
+    --concat $ replicate 10 "ha"
+    buildParagraph(randomWord(list), list)
